5f8634dc68bfc5992cea61d3d071ba33bc9678b1
SeifEldinMohamed <seifeldinmohamed101@gmail.com>
2022-10-23 19:45:03 +0200

    User Class Added

--- app/src/main/java/com/seif/cleanarchitecturenoteappwithfirebase/MainActivity.kt
+++ app/src/main/java/com/seif/cleanarchitecturenoteappwithfirebase/MainActivity.kt
@@ -2,10 +2,33 @@ package com.seif.cleanarchitecturenoteappwithfirebase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.seif.cleanarchitecturenoteappwithfirebase.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val TAG = "MainActivity"
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
+/*
+*       for(i in 0..6){
+//           val user =  User(
+//                "User $i",
+//                "user$i@gmail.com",
+//                "user_$i",
+//                i%2 ==0
+//            )
+//            FirebaseFirestore.getInstance().collection("users")
+//                .add(user)
+//                .addOnSuccessListener { documentReference ->
+//                    Log.d("Main", "onCreate: Document Snapshot added with id: $documentReference")
+//                }
+//                .addOnFailureListener{ e ->
+//                    Log.d("Main", "onCreate: Error Adding Document $e")
+//                }
+//        }
+* */
---
+++ app/src/main/java/com/seif/cleanarchitecturenoteappwithfirebase/User.kt
@@ -0,0 +1,8 @@
+package com.seif.cleanarchitecturenoteappwithfirebase
+
+data class User(
+    val name: String,
+    val email: String,
+    val password: String,
+    val subscribed: Boolean
+)
