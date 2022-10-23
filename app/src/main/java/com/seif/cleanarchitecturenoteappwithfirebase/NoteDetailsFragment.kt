22578885fd2389baac59f03d9f415748252cf01a
SeifEldinMohamed <seifeldinmohamed101@gmail.com>
2022-10-23 20:09:55 +0200

    Navigation Component Added

---
+++ app/src/main/java/com/seif/cleanarchitecturenoteappwithfirebase/NoteDetailsFragment.kt
@@ -0,0 +1,26 @@
+package com.seif.cleanarchitecturenoteappwithfirebase
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.seif.cleanarchitecturenoteappwithfirebase.databinding.FragmentNoteDetailsBinding
+
+class NoteDetailsFragment : Fragment() {
+    private val TAG = "NoteDetailsFragment"
+    lateinit var binding: FragmentNoteDetailsBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentNoteDetailsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+}
---
+++ app/src/main/java/com/seif/cleanarchitecturenoteappwithfirebase/NoteListFragment.kt
@@ -0,0 +1,26 @@
+package com.seif.cleanarchitecturenoteappwithfirebase
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.seif.cleanarchitecturenoteappwithfirebase.databinding.FragmentNoteListBinding
+
+class NoteListFragment : Fragment() {
+    private val TAG = "NoteListFragment"
+    private lateinit var binding: FragmentNoteListBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentNoteListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+}
