1fc54fd97d8badbff675266b3f5e06c897359845
SeifEldinMohamed <seifeldinmohamed101@gmail.com>
2022-12-09 12:26:58 +0200

    - UploadMultipleImagesUseCase.kt and UploadSingleImageUseCase.kt Added - edit AddNoteUseCase.kt

--- app/src/main/java/com/seif/cleanarchitecturenoteappwithfirebase/domain/usecase/AddNoteUseCase.kt
+++ app/src/main/java/com/seif/cleanarchitecturenoteappwithfirebase/domain/usecase/AddNoteUseCase.kt
@@ -4,18 +4,15 @@ import com.seif.cleanarchitecturenoteappwithfirebase.domain.model.Note
 import com.seif.cleanarchitecturenoteappwithfirebase.domain.repository.NoteRepository
 import com.seif.cleanarchitecturenoteappwithfirebase.utils.Resource
 import com.seif.cleanarchitecturenoteappwithfirebase.utils.validNote
-import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 class AddNoteUseCase @Inject constructor(
     private val noteRepository: NoteRepository
 ) {
-    operator fun invoke(note: Note) = flow {
-        when (val result = note.validNote()) {
-            is Resource.Error -> emit(result)
-            is Resource.Success -> noteRepository.addNote(note).collect {
-                emit(it)
-            }
+    suspend operator fun invoke(note: Note): Resource<String, String> {
+        return when (val result = note.validNote()) {
+            is Resource.Error -> result
+            is Resource.Success -> noteRepository.addNote(note)
         }
     }
 }
--- app/src/main/java/com/seif/cleanarchitecturenoteappwithfirebase/domain/usecase/UploadMultipleImagesUseCase.kt
+++ app/src/main/java/com/seif/cleanarchitecturenoteappwithfirebase/domain/usecase/UploadMultipleImagesUseCase.kt
@@ -3,13 +3,15 @@ package com.seif.cleanarchitecturenoteappwithfirebase.domain.usecase
 import android.net.Uri
 import com.seif.cleanarchitecturenoteappwithfirebase.domain.repository.NoteRepository
 import com.seif.cleanarchitecturenoteappwithfirebase.utils.Resource
-import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
-class UploadSingleImageUseCase @Inject constructor(
+class UploadMultipleImagesUseCase @Inject constructor(
     private val noteRepository: NoteRepository
 ) {
-    suspend operator fun invoke(imageUri: Uri): Flow<Resource<Uri, String>> {
-        return noteRepository.uploadSingleImage(imageUri)
+    suspend operator fun invoke(imagesUri: List<Uri>): Resource<List<Uri>, String> {
+        return if (imagesUri.isNotEmpty())
+            noteRepository.uploadMultipleImages(imagesUri)
+        else
+            Resource.Error("You have to upload at least one image.")
     }
 }
